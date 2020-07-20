2079e6596bb40d8c4c57b99ec66a8ac78ed0480c
Kiara Sims <simskiara6@gmail.com>
2020-07-20 01:45:31 -0500

    update

--- todo-list/src/main/java/com/google/sps/servlets/DeleteTaskServlet.java
+++ todo-list/src/main/java/com/google/sps/servlets/DeleteTaskServlet.java
@@ -23,6 +23,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.sps.servlets.NewTaskServlet;
+import com.google.sps.servlets.ListTasksServlet;
+
 
 /** Servlet responsible for deleting tasks. */
 @WebServlet("/delete-task")
